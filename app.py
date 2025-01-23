34cff8465dc0e108e44406be15819341a37ee941
oleoespindola <leo0.moreirah@gmail.com>
2025-01-23 18:14:37 -0300

    add status, sectors& users

--- app.py
+++ app.py
@@ -72,9 +72,9 @@ def upsert_history(created_from: str, created_to: str) -> pd.DataFrame:
                 conn.execute(text('DROP TABLE IF EXISTS dball.temp_history'))
                 conn.commit()
         except exc.IntegrityError:
-            Telegram('*dbmkt* | Foreign Key error in history')
+            Telegram('dbmkt | Foreign Key error in history')
         except Exception:
-            Telegram('*dbmkt* | General error')
+            Telegram('dbmkt | General error')
         
 def get_messages(created_from: str, created_to: str):
     header = get_header()
@@ -109,14 +109,177 @@ def upsert_messages(created_from: str, created_to: str):
                 conn.execute(text('DROP TABLE IF EXISTS dball.temp_messages'))
                 conn.commit()
         except exc.IntegrityError:
-            Telegram('*dbmkt* | Foreign Key error in messages')
+            Telegram('dbmkt | Foreign Key error in messages')
         except Exception:
-            Telegram('*dbmkt* | General error')
+            Telegram('dbmkt | General error')
 
+def get_sectors():
+    header = get_header()
+    response = requests.get(URL + '/sector', headers=header)
+    df = pd.DataFrame(response.json())
+    if df.empty:
+        return df
+    df['created_at'] = pd.to_datetime(df['created_at'], format=STR_DATE_FORMAT)
+    df['updated_at'] = pd.to_datetime(df['updated_at'], format=STR_DATE_FORMAT)
+    df['deleted_at'] = pd.to_datetime(df['deleted_at'], format=STR_DATE_FORMAT)
+    return df
+
+def upsert_sectors():
+    df = get_sectors()
+    if not df.empty:
+        try:
+            print('Aguarde, Atualizando setores...')
+            engine = create_engine(get_str_engine())
+            Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+            session = Session()
+            
+            query = f"""
+                INSERT INTO dbmkt.sectors ({', '.join(df.columns)})
+                SELECT * FROM dball.temp_sectors
+                ON DUPLICATE KEY UPDATE
+                    {', '.join([f'{col} = VALUES({col})' for col in df.columns])}
+            """
+            
+            with session.connection() as conn:
+                df.to_sql(name='temp_sectors', schema='dball', con=conn, if_exists='replace', index=False)    
+                conn.commit()
+                conn.execute(text(query))
+                conn.execute(text('DROP TABLE IF EXISTS dball.temp_sectors'))
+                conn.commit()
+            
+            print('✅ Setores atualizados!')
+        except exc.IntegrityError:
+            Telegram('dbmkt | Foreign Key error in sectors')
+        except Exception:
+            Telegram('dbmkt | General error in upsert sectors')
+
+def get_status():
+    header = get_header()
+    response = requests.get(URL + '/status', headers=header)
+    df = pd.DataFrame(response.json())
+    if df.empty:
+        return df
+    df['created_at'] = pd.to_datetime(df['created_at'], format=STR_DATE_FORMAT)
+    df['updated_at'] = pd.to_datetime(df['updated_at'], format=STR_DATE_FORMAT)
+    df['deleted_at'] = pd.to_datetime(df['deleted_at'], format=STR_DATE_FORMAT)
+    return df
+
+def upsert_status():
+    df = get_status()
+    if not df.empty:
+        try:
+            print('Aguarde, atualizando status...', end='\r')
+            engine = create_engine(get_str_engine())
+            Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+            session = Session()
+            
+            query = f"""
+                INSERT INTO dbmkt.status ({', '.join(df.columns)})
+                SELECT * FROM dball.temp_status
+                ON DUPLICATE KEY UPDATE
+                    {', '.join([f'{col} = VALUES({col})' for col in df.columns])}
+            """
+            
+            with session.connection() as conn:
+                df.to_sql(name='temp_status', schema='dball', con=conn, if_exists='replace', index=False)
+                conn.commit()
+                conn.execute(text(query))
+                conn.execute(text('DROP TABLE IF EXISTS dball.temp_status'))
+                conn.commit()
+            
+            print('✅ Status atualizados!')
+        except exc.IntegrityError:
+            Telegram('dbmkt | Foreign Key error in status')
+        except Exception:
+            Telegram('dbmkt | General error in upsert status')
+            
+def get_sectors():
+    header = get_header()
+    response = requests.get(URL + '/sector', headers=header)
+    df = pd.DataFrame(response.json())
+    if df.empty:    
+        return df
+    df['created_at'] = pd.to_datetime(df['created_at'], format=STR_DATE_FORMAT)
+    df['updated_at'] = pd.to_datetime(df['updated_at'], format=STR_DATE_FORMAT)
+    df['deleted_at'] = pd.to_datetime(df['deleted_at'], format=STR_DATE_FORMAT)
+    return df
+
+def upsert_sectors():
+    df = get_sectors()
+    if not df.empty:
+        try:
+            print('Aguarde, Atualizando setores...', end='\r')
+            engine = create_engine(get_str_engine())
+            Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+            session = Session()
+            
+            query = f"""
+                INSERT INTO dbmkt.sectors ({', '.join(df.columns)})
+                SELECT * FROM dball.temp_sectors
+                ON DUPLICATE KEY UPDATE
+                    {', '.join([f'{col} = VALUES({col})' for col in df.columns])}
+            """
+            
+            with session.connection() as conn:
+                df.to_sql(name='temp_sectors', schema='dball', con=conn, if_exists='replace', index=False)    
+                conn.commit()
+                conn.execute(text(query))
+                conn.execute(text('DROP TABLE IF EXISTS dball.temp_sectors'))
+                conn.commit()
+            
+            print('✅ Setores atualizados!')
+        except exc.IntegrityError:
+            Telegram('dbmkt | Foreign Key error in sectors')
+        except Exception:
+            Telegram('dbmkt | General error in upsert sectors')
+
+def get_users():
+    header = get_header()
+    response = requests.get(URL + '/user', headers=header)
+    df = pd.DataFrame(response.json())
+    if df.empty:
+        return df
+    df['created_at'] = pd.to_datetime(df['created_at'], format=STR_DATE_FORMAT)
+    df['inactive_since'] = pd.to_datetime(df['inactive_since'], format=STR_DATE_FORMAT)
+    return df
+
+def upsert_user():
+    df = get_users()
+    if not df.empty:
+        try:
+            print('Aguarde, atualizando usuários...', end='\r')
+            engine = create_engine(get_str_engine())
+            Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+            session = Session()
+            
+            query = f"""
+                INSERT INTO dbmkt.users ({', '.join(df.columns)})
+                SELECT * FROM dball.temp_users
+                ON DUPLICATE KEY UPDATE
+                    {', '.join([f'{col} = VALUES({col})' for col in df.columns])}
+            """
+            
+            with session.connection() as conn:
+                df.to_sql(name='temp_users', schema='dball', con=conn, if_exists='replace', index=False)
+                conn.commit()
+                conn.execute(text(query))
+                conn.execute(text('DROP TABLE IF EXISTS dball.temp_users'))
+                conn.commit()
+            
+            print('✅ Usuários atualizados!')
+        except exc.IntegrityError:
+            Telegram('dbmkt | Foreign Key error in users')
+        except Exception:
+            Telegram('dbmkt | General error in upsert users')
+            
 def main():
     
+    upsert_sectors()
+    upsert_status()
+    upsert_user()
+    
     end_date = datetime.now()
-    number_of_days = 30
+    number_of_days = 400
     while number_of_days > 0:
         create_from = (end_date - pd.Timedelta(days=number_of_days)).strftime('%Y-%m-%d')
         created_to = (end_date - pd.Timedelta(days=number_of_days-1)).strftime('%Y-%m-%d')
